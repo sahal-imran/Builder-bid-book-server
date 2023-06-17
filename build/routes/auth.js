54971aac44c178c6b9fec00d37fab497ade65b0d
Sahal Imran <sahalimran786@gmail.com>
2023-06-18 01:02:53 +0500

    Sign up route added

--- build/routes/auth.js
+++ build/routes/auth.js
@@ -13,35 +13,41 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-// import bcrypt from "bcrypt"
+const Log_1 = require("../utils/Log");
+const User_1 = __importDefault(require("../models/User"));
+const MongoDBErrorController_1 = __importDefault(require("../utils/MongoDBErrorController"));
+const sendMail_1 = __importDefault(require("../lib/sendMail"));
+const bcrypt_1 = __importDefault(require("bcrypt"));
 // Instances
 const router = express_1.default.Router();
 // Sign up
-// router.get("/signup", async (req: Request, res: Response) => {
-//     try {
-//         const body = req.body;
-//         const password = await bcrypt.hash(body.password, 12)
-//         const createUser = await User.create({ ...body, password });
-//         sendMail(createUser?.companyEmail, "Welcome", "Hi Welcome to the app", (error) => {
-//             if (error) LogError("/signup(sendMail)", error)
-//             res.status(201).json({ message: "Registered" })
-//         })
-//     } catch (error) {
-//         const errorHandler: any = MongoDBErrorController(error)
-//         if (errorHandler.status === 403) {
-//             LogWarning("/signup(Validation)", errorHandler.message)
-//             res.status(errorHandler.status).json({ message: errorHandler.message })
-//         }
-//         else if (errorHandler.status === 409) {
-//             LogWarning("/signup(Duplication)", errorHandler.message)
-//             res.status(errorHandler.status).json({ message: errorHandler.message })
-//         }
-//         else {
-//             LogError("/signup", error)
-//             res.status(500).json({ message: "Server error" })
-//         }
-//     }
-// });
+router.get("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const body = req.body;
+        const password = yield bcrypt_1.default.hash(body.password, 12);
+        const createUser = yield User_1.default.create(Object.assign(Object.assign({}, body), { password }));
+        (0, sendMail_1.default)(createUser === null || createUser === void 0 ? void 0 : createUser.companyEmail, "Welcome", "Hi Welcome to the app", (error) => {
+            if (error)
+                (0, Log_1.LogError)("/signup(sendMail)", error);
+            res.status(201).json({ message: "Registered" });
+        });
+    }
+    catch (error) {
+        const errorHandler = (0, MongoDBErrorController_1.default)(error);
+        if (errorHandler.status === 403) {
+            (0, Log_1.LogWarning)("/signup(Validation)", errorHandler.message);
+            res.status(errorHandler.status).json({ message: errorHandler.message });
+        }
+        else if (errorHandler.status === 409) {
+            (0, Log_1.LogWarning)("/signup(Duplication)", errorHandler.message);
+            res.status(errorHandler.status).json({ message: errorHandler.message });
+        }
+        else {
+            (0, Log_1.LogError)("/signup", error);
+            res.status(500).json({ message: "Server error" });
+        }
+    }
+}));
 // Sign up
 router.get("/login", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,38 +3,38 @@ import { LogError, LogWarning } from "../utils/Log";
 import User from "../models/User";
 import MongoDBErrorController from '../utils/MongoDBErrorController';
 import sendMail from '../lib/sendMail';
-// import bcrypt from "bcrypt"
+import bcrypt from "bcrypt"
 
 
 // Instances
 const router = express.Router();
 
 // Sign up
-// router.get("/signup", async (req: Request, res: Response) => {
-//     try {
-//         const body = req.body;
-//         const password = await bcrypt.hash(body.password, 12)
-//         const createUser = await User.create({ ...body, password });
-//         sendMail(createUser?.companyEmail, "Welcome", "Hi Welcome to the app", (error) => {
-//             if (error) LogError("/signup(sendMail)", error)
-//             res.status(201).json({ message: "Registered" })
-//         })
-//     } catch (error) {
-//         const errorHandler: any = MongoDBErrorController(error)
-//         if (errorHandler.status === 403) {
-//             LogWarning("/signup(Validation)", errorHandler.message)
-//             res.status(errorHandler.status).json({ message: errorHandler.message })
-//         }
-//         else if (errorHandler.status === 409) {
-//             LogWarning("/signup(Duplication)", errorHandler.message)
-//             res.status(errorHandler.status).json({ message: errorHandler.message })
-//         }
-//         else {
-//             LogError("/signup", error)
-//             res.status(500).json({ message: "Server error" })
-//         }
-//     }
-// });
+router.get("/signup", async (req: Request, res: Response) => {
+    try {
+        const body = req.body;
+        const password = await bcrypt.hash(body.password, 12)
+        const createUser = await User.create({ ...body, password });
+        sendMail(createUser?.companyEmail, "Welcome", "Hi Welcome to the app", (error) => {
+            if (error) LogError("/signup(sendMail)", error)
+            res.status(201).json({ message: "Registered" })
+        })
+    } catch (error) {
+        const errorHandler: any = MongoDBErrorController(error)
+        if (errorHandler.status === 403) {
+            LogWarning("/signup(Validation)", errorHandler.message)
+            res.status(errorHandler.status).json({ message: errorHandler.message })
+        }
+        else if (errorHandler.status === 409) {
+            LogWarning("/signup(Duplication)", errorHandler.message)
+            res.status(errorHandler.status).json({ message: errorHandler.message })
+        }
+        else {
+            LogError("/signup", error)
+            res.status(500).json({ message: "Server error" })
+        }
+    }
+});
 
 // Sign up
 router.get("/login", async (req, res) => {
